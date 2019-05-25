9b3e8691cf86e45003cf9a63e1fc9583a2d157b9
Alethea Cary <aletheacary@gmail.com>
2019-05-25 23:45:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    self.new_from_filename(filename)
+    @@all << self.new_from_filename(filename)
     #self.find_or_create_by_name(filename)
     #song_match = self.find_by_name(name)
     #song_match.name = name
